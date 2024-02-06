56c68feafd90e131285f84557c0854d76dc5c4fe
Aaditya Raj Anand <adityaraj912830@gmail.com>
2024-02-06 17:50:07 +0530

    add mobile navbar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -23,18 +23,67 @@
         </div>
       </div>
     </div>
+
+    <!-- Hamburger Menu for Small Screens -->
+    <div class="md:hidden flex sticky top-0 z-50 items-center justify-around" 
+          style=" width:100%;">
+          <div style="width: 36px; height: 32px;">
+          </div>
+            <NuxtLink @click="toggleMobileMenu();" to="/" style="z-index: 3; !important">
+              <img height="120vh" width="110vh" src="../assets/dhanak_logo.png">
+            </NuxtLink>
+            <button id="mobile-menu-btn" style=" z-index: 3;"
+              class="text-white hover:text-gray-300 focus:outline-none focus:text-gray-300">
+              <input id="checkbox" type="checkbox" v-model="isMobileMenuVisible" @click="toggleMobileMenu();">
+                <label class="toggle" for="checkbox">
+                    <div id="bar1" class="bars"></div>
+                    <div id="bar2" class="bars"></div>
+                    <div id="bar3" class="bars"></div>
+                </label>
+            </button>
+    </div>
   </header>
+  <!-- Mobile Menu (Hidden by default) -->
+  <div v-show="isMobileMenuVisible"
+        class=" md:hidden w-full left-0 right-0 grid items-center justify-center"
+        style=" position: absolute; top:0; height:100%; z-index: 2; background-color: rgba(4, 4, 36, 0.86);">
+        <div class="grid">
+          <NuxtLink @click="toggleMobileMenu();" class="mt-4 mobileNavbarLink" to="/">Home</NuxtLink>
+          <NuxtLink @click="toggleMobileMenu();" class="mt-4 mobileNavbarLink" to="/about">About</NuxtLink>
+          <NuxtLink @click="toggleMobileMenu();" class="mt-4 mobileNavbarLink" to="/events">Events</NuxtLink>
+          <NuxtLink @click="toggleMobileMenu();" class="mt-4 mobileNavbarLink" to="/registration">SignUp</NuxtLink>
+          <NuxtLink @click="toggleMobileMenu();" class="mt-4 mobileNavbarLink" to="/registration">LogIn</NuxtLink>
+          <NuxtLink @click="toggleMobileMenu();" class="mt-4 mobileNavbarLink" to="/merchandise">Merchandise</NuxtLink>   
+        </div>
+    </div>
 </template>
 
+<script>
+  export default {
+  data() {
+    return {
+      isMobileMenuVisible: false,
+    };
+  },
+
+  methods: {
+    toggleMobileMenu() {
+      this.isMobileMenuVisible = !this.isMobileMenuVisible;
+    },
+  }
+};
+</script>
+
 <style scoped>
-    #navbar{
-  background: rgba(4, 4, 36, 0.86);
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5.1px);
-  -webkit-backdrop-filter: blur(5.1px);
-  border: 1px solid rgba(4, 4, 36, 0.24);
+  #navbar{
+    background: rgba(4, 4, 36, 0.86);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5.1px);
+    -webkit-backdrop-filter: blur(5.1px);
+    border: 1px solid rgba(4, 4, 36, 0.24);
 }
-    li{
+
+li{
     margin-left: 2.1rem;
     margin-right: 2.1rem;
     font-weight: 600;
@@ -44,7 +93,7 @@
     border: none;
     background: none;
     font-size: 16px;
-  }
+}
 
 li::before {
   margin-left: auto;
@@ -62,4 +111,87 @@ li::after, li::before {
 li:hover::after, li:hover::before {
   width: 100%;
 }
+
+
+#checkbox {
+  display: none;
+}
+
+.toggle {
+  position: relative;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  transition-duration: .5s;
+}
+
+.bars {
+  width: 100%;
+  height: 5px;
+  background-color: #EC9420;
+  border-radius: 4px;
+}
+
+#bar2 {
+  transition-duration: .8s;
+}
+
+#bar1,#bar3 {
+  width: 70%;
+}
+
+#checkbox:checked + .toggle .bars {
+  position: absolute;
+  transition-duration: .5s;
+}
+
+#checkbox:checked + .toggle #bar2 {
+  transform: scaleX(0);
+  transition-duration: .5s;
+}
+
+#checkbox:checked + .toggle #bar1 {
+  width: 100%;
+  transform: rotate(45deg);
+  transition-duration: .5s;
+}
+
+#checkbox:checked + .toggle #bar3 {
+  width: 100%;
+  transform: rotate(-45deg);
+  transition-duration: .5s;
+}
+
+#checkbox:checked + .toggle {
+  transition-duration: .5s;
+  transform: rotate(180deg);
+}
+  .mobileNavbarLink {
+    /* */
+    width: 15rem;
+    font-size: 1.2rem;
+  padding: 1rem 2.5rem;
+  border: none;
+  outline: none;
+  border-radius: 0.4rem;
+  cursor: pointer;
+  font-family: 'forgotten-futurist-bd';
+  text-transform: uppercase;
+  background-color: rgb(14, 14, 26);
+  color: rgb(255, 255, 255);
+  font-weight: 800;
+  /* box-shadow: 0px 0px 60px #1C9C8C; */
+  }
+
+  .mobileLogo{
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
 </style>
